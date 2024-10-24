ba59441ea2b01f0cfa0ee197599a747c087d33a0
Andrew-54 <andrewkdavis54@gmail.com>
2024-10-24 16:28:59 -0700

    say

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void say(string);
+
 int main()
 {
     cout << "Greetings\n";
     cout << "Hello, World\n";
+    say("hi");
     return 0;
+}
+
+void say(string s)
+{
+    cout << s << endl;
 }
