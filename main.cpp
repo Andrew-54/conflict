a3fdbbb8a18e6874f7a780fc7c4e78071812a905
Andrew-54 <andrewkdavis54@gmail.com>
2024-10-24 23:25:01 +0000

    Sum

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
+int add(int a, int b);
 
 int main()
 {
     cout << "Hello, World\n";
+    cout << "The sum of 3 and 4 is " << add(3,4) << endl;
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a+b;
 }
